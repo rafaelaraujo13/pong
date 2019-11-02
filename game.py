6bd3ffdb1700c712732bf4e78389f407327a1e6c
Rafael Araujo <araujorafael9201@gmail.com>
2019-11-02 18:02:10 -0300

    desenha raquetes estáticas

--- game.py
+++ game.py
@@ -9,18 +9,31 @@ except:
 # variáveis principais
 rodando = True
 branco = (255, 255, 255)
+preto = (0, 0, 0)
 
 # configurando tela
 altura_tela = 480
 comprimento_tela = 640
 tela = pygame.display.set_mode((comprimento_tela, altura_tela))
 
+# raquetes
+def raquete(lado):
+    if lado == 'esquerda':
+        pygame.draw.rect(tela, preto, (0, (altura_tela / 2) - 50, 20, 100))
+    elif lado == 'direita':
+        pygame.draw.rect(tela, preto, (comprimento_tela - 20, (altura_tela / 2) - 50, 20, 100))
+
 # loop do jogo
 while rodando:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             rodando = False
-
+    
     tela.fill(branco)
-
+    
+    # cria as raquetes
+    raquete('esquerda')
+    raquete('direita')
+    
+    
     pygame.display.update()
