9af22896125e2747f464bdc22fe413c4d4ae6ea0
Rafael Araujo <araujorafael9201@gmail.com>
2019-11-02 18:08:11 -0300

    raquete do jogador se movendo

--- game.py
+++ game.py
@@ -10,16 +10,21 @@ except:
 rodando = True
 branco = (255, 255, 255)
 preto = (0, 0, 0)
+clock = pygame.time.Clock()
 
 # configurando tela
 altura_tela = 480
 comprimento_tela = 640
 tela = pygame.display.set_mode((comprimento_tela, altura_tela))
 
+# config da raquete do jogador
+pos_y = (altura_tela / 2) - 50
+vel = 0
+
 # raquetes
 def raquete(lado):
     if lado == 'esquerda':
-        pygame.draw.rect(tela, preto, (0, (altura_tela / 2) - 50, 20, 100))
+        pygame.draw.rect(tela, preto, (0, pos_y, 20, 100))
     elif lado == 'direita':
         pygame.draw.rect(tela, preto, (comprimento_tela - 20, (altura_tela / 2) - 50, 20, 100))
 
@@ -28,12 +33,21 @@ while rodando:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             rodando = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                vel = -20
+            elif event.key == pygame.K_DOWN:
+                vel = 20 
     
     tela.fill(branco)
     
     # cria as raquetes
+
+    pos_y += vel
+
     raquete('esquerda')
     raquete('direita')
     
     
     pygame.display.update()
+    clock.tick(30)
